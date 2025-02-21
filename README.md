19e403876f2cc03a7be9d172af4107d6bb16ee90
David Herney <davidherney@gmail.com>
2025-02-21 15:37:46 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Project contents: SGR-IA - IA para aprender.
+
+This repository contains the educational materials created for the project,
+in their different versions, including the packages compatible with the SCORM 1.2 standard.
+
+## License ##
+
+Contents developed within the framework of the SGRAD project.
+
+Project financed by the General Royalties System of Colombia and executed by
+the Universidad de Antioquia. BPIN-2021000100186.
+
+This work is licensed under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)
+![CC BY 4.0](https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1)
+
+[Tepuy template](https://tepuy.bambuco.co/) is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+Tepuy template is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <https://www.gnu.org/licenses/>.
---
+++ tema4-logicaproposicionalycuantificacional/components/learningstyles/lib.js
@@ -0,0 +1,126 @@
+// This file is part of Tepuy template.
+//
+// Tepuy is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Tepuy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Tepuy.  If not, see <http://www.gnu.org/licenses/>.
+
+(function($){
+    "use strict";
+
+    var M = null;
+    var siteurl = null;
+    var defaultsource = '';
+    var globalstyles = [];
+    var cyclelearning = {
+        'grassping': [],
+        'transforming': []
+    };
+
+    cyclelearning.grassping['input'] = false;
+    cyclelearning.grassping['perception'] = false;
+    cyclelearning.transforming['processing'] = false;
+    cyclelearning.transforming['understanding'] = false;
+
+    if (parent && parent.window.M) {
+        M = parent.window.M;
+
+        if (parent.window.scormplayerdata) {
+            siteurl = M.cfg.wwwroot;
+            defaultsource = siteurl + '/user/profile/field/learningstyles/get.php';
+        }
+    }
+
+    $.learningStyles = {
+        'options': {
+            'source': defaultsource,
+        },
+
+        'get': function(callback, newoptions) {
+
+            
+            var response = {
+                'styles': null,
+                'cycles': cyclelearning
+            };
+
+            callback(response);
+            return;
+            
+            var options = this.options = $.extend(this.options, newoptions);
+            var response = null;
+
+            if (options.source == '') {
+                if (callback && typeof callback == 'function') {
+                    callback(response);
+                }
+            }
+
+            if (globalstyles[options.source]) {
+                if (callback && typeof callback == 'function') {
+                    callback(globalstyles[options.source]);
+                    return;
+                }
+            }
+
+            $.get(options.source, function(data) {
+
+                if (data) {
+
+                    if (typeof data == 'object'
+                        && data.input
+                        && data.perception
+                        && data.processing
+                        && data.understanding
+                    ) {
+
+                        if (data.input < 0) {
+                            cyclelearning.grassping['input'] = 'visual';
+                        } else {
+                            cyclelearning.grassping['input'] = 'verbal';
+                        }
+
+                        if (data.perception < 0) {
+                            cyclelearning.grassping['perception'] = 'sensing';
+                        } else {
+                            cyclelearning.grassping['perception'] = 'intuitive';
+                        }
+
+                        if (data.processing < 0) {
+                            cyclelearning.transforming['processing'] = 'active';
+                        } else {
+                            cyclelearning.transforming['processing'] = 'reflective';
+                        }
+
+                        if (data.understanding < 0) {
+                            cyclelearning.transforming['understanding'] = 'sequential';
+                        } else {
+                            cyclelearning.transforming['understanding'] = 'global';
+                        }
+
+                        globalstyles[options.source] = {
+                            'styles': data,
+                            'cycles': cyclelearning
+                        };
+
+                        response = globalstyles[options.source];
+                    }
+                }
+
+                if (callback && typeof callback == 'function') {
+                    callback(response);
+                }
+            });
+        },
+
+    };
+
+})(jQuery);
